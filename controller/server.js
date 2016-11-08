00eae478a64cf76ad46037e376c174f4fab75b67
Cedric Denoo <cedric.denoo@vub.ac.be>
2016-11-08 22:05:02 +0100

    Changed settings in server,js

--- controller/server.js
+++ controller/server.js
@@ -8,8 +8,7 @@ var path = require('path');
 var RestobarApp = function() {
     var self = this;
     self.setupVariables = function () {
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        self.port = process.env.PORT || 8080;
 
         if (typeof self.ipaddress === "undefined") {
             console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
@@ -79,9 +78,9 @@ var RestobarApp = function() {
     };
 
     self.start = function () {
-        self.app.listen(self.port, self.ipaddress, function () {
+        self.app.listen(self.port, function () {
             console.log('%s: Node server started on %s:%d ...',
-                Date(Date.now()), self.ipaddress, self.port);
+                Date(Date.now()), self.port);
         });
     };
 };
