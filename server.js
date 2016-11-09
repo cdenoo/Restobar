98f46817fef9c2c44a9cc667bd51cbeffee7e0c8
cdenoo <cc.dd.colony88@gmail.com>
2016-11-09 17:53:54 +0100

    Changes to master

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.$PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var routes = { };
 
