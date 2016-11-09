a91644c43360c0037267c29f0579fa4e916e1d43
cdenoo <cc.dd.colony88@gmail.com>
2016-11-09 17:51:20 +0100

    Changes to master

--- server.js
+++ server.js
@@ -2,54 +2,41 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 
-var RestobarApp = function(){
-	
-	this.initVariables = function(){
-		this.port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-		this.ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var app = express();
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var routes = { };
+
+createRoutes = function(){
+	routes['/'] = function(req, res, next) {
+		res.send('INDEX TODO');
 	};
-	
-	this.createRoutes = function(){
-		this.routes = { };
-		
-		this.routes['/'] = function(req, res, next) {
-            res.send('INDEX TODO');
-        };
-	};
-	
-	this.initRoutes = function(){
-		this.createRoutes();
-		
-		for (var r in this.routes) {
-			this.app.get(r, this.routes[r]);
-		};
-	};
-	
-	this.initErrorHandling = function(){
-		this.app.use(function(err, req, res, next){
-			console.error(err.stack);
-			res.status(500).send('Something bad happened!');
-		});
-	};
-	
-	this.initServer = function(){
-		// Create server
-		this.app = express();
-		// Initialize the routes
-		this.initRoutes();
-		// Initialize handling of errors
-		this.initErrorHandling();
-		// Make the server listen
-		this.app.listen(this.port, this.ip);
-	};
-	
-	this.start = function(){
-		this.initVariables();
-		this.initServer();
-		module.exports = this.app;
-	};
-	
 };
 
-var rba = new RestobarApp();
-rba.start();
+initRoutes = function(){
+	createRoutes();
+		
+	for (var r in this.routes) {
+		app.get(r, this.routes[r]);
+	};
+};
+
+initErrorHandling = function(){
+	app.use(function(err, req, res, next){
+		console.error(err.stack);
+		res.status(500).send('Something bad happened!');
+	});
+};
+
+initServer = function(){
+	// Initialize the routes
+	initRoutes();
+	// Initialize handling of errors
+	initErrorHandling();
+	// Make the server listen
+	app.listen(port, ip);
+};
+
+initServer();
+module.exports = app;
