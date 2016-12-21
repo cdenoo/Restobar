1595863a22519b55ec168f4cd2c93f9a8f7d63e5
thijsspinoy <thijs.spinoy@gmail.com>
2016-12-21 13:23:14 +0100

    Push voor Facebooklogin.

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@ module.exports = function (restobar) {
 
     restobar._app.get('/auth/facebook', restobar.passport.authenticate('facebook'));
 
-    restobar._app.get('/auth/facebook/callback', restobar.passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
+    restobar._app.get('/auth/facebook/callback',
+        restobar.passport.authenticate('facebook', { failureRedirect: '/login' }),
+        function(req, res){
+        res.redirect('/');
+    });
 
 };
