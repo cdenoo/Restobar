bf635bd42975c8dcc7f7f816c8c4317ce25f4144
thijsspinoy <thijs.spinoy@gmail.com>
2016-12-21 15:23:32 +0100

    Weather added.

--- public/scripts/weather.js
+++ public/scripts/weather.js
@@ -1,7 +1,7 @@
 var temp;
 var description;
 
-function getWeather(lat, lon) {
+function getWeather(lat, lon, $cont) {
     $.ajax({ //Shorthand for a jquery ajax post request
         url: "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=5cba87d4a2d2b2156035e74cc3ef8a97",
         dataType: 'json',
@@ -13,6 +13,8 @@ function getWeather(lat, lon) {
             temp -= 273.15;
             console.log(temp);
             console.log(description);
+            $cont.html("The forecast for your location is: " + temp.toFixed(1) + " degrees Celsius and " + description + ".");
+
         },
         error: function(jqXHR, textstatus, errorThrown){
             console.log(errorThrown);
@@ -20,12 +22,12 @@ function getWeather(lat, lon) {
     });
 }
 
-function getLocation(){
+function getWeatherOnLocation($cont){
 
     function locationCallback(location){
         console.log(location);
         if(location){
-            getWeather(location.coords.latitude, location.coords.longitude);
+            getWeather(location.coords.latitude, location.coords.longitude, $cont);
         }
     }
 
